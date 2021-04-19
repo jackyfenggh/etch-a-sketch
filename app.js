7d6de4d206d81d6405dad15bec36f8b8f9b4042a
Jacky Feng <jackyfeng1992@gmail.com>
2021-04-19 19:29:03 +1000

    feat: Add random background colour updater on hover
    Add a basic colour palette array, random colour generator function and event listener on grid container.

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
-function generateSquareDivs(dimensions) {
+var colourPalette = ['black', 'blue', 'brown', 'green', 'gray', 'yellow', 'pink', 'orange', 'red', 'purple', 'white'];
+
+function randomColourGenerator() {
+  var randomArrayIndex = Math.floor(Math.random() * colourPalette.length);
+  return colourPalette[randomArrayIndex];
+}
+
+function renderGrid(dimensions) {
   var gridContainer = document.getElementById('grid-container');
   gridContainer.innerHTML = '';
   
   for (var i = 0; i < dimensions * dimensions; i++) {
     var squareDiv = document.createElement('div');
-    squareDiv.style.border = "1px solid black";
+    squareDiv.classList.add('grid-unit');
     gridContainer.appendChild(squareDiv);
   }
 
   gridContainer.style.gridTemplateColumns = `repeat(${dimensions}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${dimensions}, 1fr)`;
-  
-  // create an array with the values to be joined
-  var gridTemplateAreasArray = [];
-  var gridTemplateAreasString = '';
-  // for loop to create each row/string for grid-template-areas value
-  for (var i = 0; i < dimensions; i++) {
-    var gridTemplateAreasArrayItem = '';
-    // for loop to populate the length of each row/string according to dimensions input by user
-    for (var j = 0; j < dimensions; j++) {
-        gridTemplateAreasArrayItem += '. ';
-    }
-    gridTemplateAreasArray.push(gridTemplateAreasArrayItem);
-  }
-  // wrap each value from the array above in "" (quotes) and join with a space in between each and wrap the final value in `` (backtick characters)
-  var gridTemplateAreasStringGenerator = function() {
-    for (var i = 0; i < gridTemplateAreasArray.length; i++) {
-      gridTemplateAreasString += `"${gridTemplateAreasArray[i]}" `;
-    }
-    gridTemplateAreasString = gridTemplateAreasString.trim();
-    gridContainer.style.gridTemplateAreas = gridTemplateAreasString;
-  }();
-  // lines 28-34 replace the need to use .map() and .join():
-  // gridTemplateAreasArray = gridTemplateAreasArray.map(i => `"${i}"`);
-  // gridContainer.style.gridTemplateAreas = gridTemplateAreasArray.join(' ');
+
+  gridContainer.addEventListener('mouseover', function(event) {
+    event.target.style.background = randomColourGenerator();
+  });
 }
-generateSquareDivs(3);
+
+
+
+
+
+renderGrid(16);
+
+
+
+// add 
+// next what I want to do: take user input and save as dimensions.
+
+// bonus
+// fix overlapping borders
+
+
+
+
+// code for grid-template-areas for later:
+  // // create an array with the values to be joined
+  // var gridTemplateAreasArray = [];
+  // var gridTemplateAreasString = '';
+  // // for loop to create each row/string for grid-template-areas value
+  // for (var i = 0; i < dimensions; i++) {
+  //   var gridTemplateAreasArrayItem = '';
+  //   // for loop to populate the length of each row/string according to dimensions input by user
+  //   for (var j = 0; j < dimensions; j++) {
+  //       gridTemplateAreasArrayItem += 'grid-unit ';
+  //   }
+  //   gridTemplateAreasArray.push(gridTemplateAreasArrayItem);
+  // }
+  // // wrap each value from the array above in "" (quotes) and join with a space in between each and wrap the final value in `` (backtick characters)
+  // var gridTemplateAreasStringGenerator = function() {
+  //   for (var i = 0; i < gridTemplateAreasArray.length; i++) {
+  //     gridTemplateAreasString += `"${gridTemplateAreasArray[i]}" `;
+  //   }
+  //   gridTemplateAreasString = gridTemplateAreasString.trim();
+  //   gridContainer.style.gridTemplateAreas = gridTemplateAreasString;
+  // }();
+  // // the code block directly above replaces the need to use .map() and .join():
+  // // gridTemplateAreasArray = gridTemplateAreasArray.map(i => `"${i}"`);
+  // // gridContainer.style.gridTemplateAreas = gridTemplateAreasArray.join(' ');
